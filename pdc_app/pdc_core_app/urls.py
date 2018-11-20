95b934d54625d152c25b28fa84ff77c5fad95bf2
plsergent <papa.louis59@gmail.com>
2018-11-20 11:01:45 +0100

    index html css

--- pdc_app/pdc_core_app/urls.py
+++ pdc_app/pdc_core_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('projets/', views.projets, name='projets'),
+    path('collaborateurs/', views.collaborateurs, name='collaborateurs'),
+    path('commandes/', views.commandes, name='commandes'),
+    path('autres/', views.autres, name='autres'),
 ]
--- pdc_app/pdc_core_app/views.py
+++ pdc_app/pdc_core_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'pdc_core_app/index.html')
+
+
+def projets(request):
+    return render(request, 'pdc_core_app/projets.html')
+
+
+def collaborateurs(request):
+    return render(request, 'pdc_core_app/collaborateurs.html')
+
+
+def commandes(request):
+    return render(request, 'pdc_core_app/commandes.html')
+
+
+def autres(request):
+    return render(request, 'pdc_core_app/autres.html')
