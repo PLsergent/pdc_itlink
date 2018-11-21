db0d67e35c6e6c85db346c154bf84e6a3bee626f
plsergent <papa.louis59@gmail.com>
2018-11-21 17:24:33 +0100

    biggest commit ever: display collab + using datatables + understanding /static/

--- pdc_app/pdc_core_app/views.py
+++ pdc_app/pdc_core_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from datetime import datetime as datet
+from dateutil import relativedelta as rd
+from .models import RepartitionProjet
 
 
 def index(request):
@@ -6,16 +9,31 @@ def index(request):
 
 
 def projets(request):
-    return render(request, 'pdc_core_app/projets.html')
+    page_title = 'Projets'
+    list_month_display = []
+    for i in range(0, 12):
+        date_month_after = datet.now() + rd.relativedelta(months=i)
+        list_month_display.append((date_month_after.month, date_month_after.year))
+    repartitions = RepartitionProjet.objects.all()
+    return render(request, 'pdc_core_app/projets.html',
+                  {'page_title': page_title,
+                   'list_month_display': list_month_display,
+                   'repartitions': repartitions})
 
 
 def collaborateurs(request):
-    return render(request, 'pdc_core_app/collaborateurs.html')
+    page_title = 'Collaborateurs'
+    return render(request, 'pdc_core_app/collaborateurs.html',
+                  {'page_title': page_title})
 
 
 def commandes(request):
-    return render(request, 'pdc_core_app/commandes.html')
+    page_title = 'Commandes'
+    return render(request, 'pdc_core_app/commandes.html',
+                  {'page_title': page_title})
 
 
 def autres(request):
-    return render(request, 'pdc_core_app/autres.html')
+    page_title = 'Autres'
+    return render(request, 'pdc_core_app/autres.html',
+                  {'page_title': page_title})
