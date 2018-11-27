d9bcefa696626377b1461790b038ef0c9b3be2eb
plsergent <papa.louis59@gmail.com>
2018-11-27 11:10:38 +0100

    js color code

--- pdc_app/pdc_core_app/static/pdc_core_app/collaborateurs.js
+++ pdc_app/pdc_core_app/static/pdc_core_app/collaborateurs.js
@@ -8,4 +8,23 @@ $(document).ready( function () {
       }
     }
     );
+    var table = new $.fn.dataTable.Api( '#myTable' );
+
+    table.cells().every( function () {
+        if ( this.data() > 100 ){
+          $(this.node()).addClass( 'has-background-danger' );
+        }
+        else if ( this.data() >= 80 ) {
+            $(this.node()).addClass( 'has-background-success' );
+        }
+        else if ( this.data() < 80 &&  this.data() >= 50 ) {
+            $(this.node()).addClass( 'has-background-primary' );
+        }
+        else if ( this.data() < 50 && this.data() != 0) {
+            $(this.node()).addClass( 'has-background-warning' );
+        }
+        else {
+            $(this.node()).removeClass( 'has-background-success' );
+        }
+    } );
 } );
--- pdc_app/pdc_core_app/static/pdc_core_app/projets.js
+++ pdc_app/pdc_core_app/static/pdc_core_app/projets.js
@@ -6,9 +6,25 @@ $(document).ready( function () {
       paging:         false,
       fixedColumns: {
         leftColumns: 7
-      }
+      },
     }
     );
+    var table = new $.fn.dataTable.Api( '#myTable' );
+
+    table.cells().every( function () {
+        if ( this.data() >= 80 ) {
+            $(this.node()).addClass( 'has-background-success' );
+        }
+        else if ( this.data() < 80 &&  this.data() >= 50 ) {
+            $(this.node()).addClass( 'has-background-primary' );
+        }
+        else if ( this.data() < 50 && this.data() != 0) {
+            $(this.node()).addClass( 'has-background-warning' );
+        }
+        else {
+            $(this.node()).removeClass( 'has-background-success' );
+        }
+    } );
 
     $('#switchAll').prop('checked', true);
     $('#switchE').prop('checked', false);
