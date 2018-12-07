7039b5bdc5b97aa37d650d8a980126196717a333
plsergent <papa.louis59@gmail.com>
2018-12-07 16:39:29 +0100

    del button ok

--- pdc_app/pdc_core_app/static/pdc_core_app/assign.js
+++ pdc_app/pdc_core_app/static/pdc_core_app/assign.js
@@ -13,10 +13,11 @@ $(document).ready(function() {
   $("#new").on('click', function(){
     var index = $("#id_form-TOTAL_FORMS").val();
     var line = (parseInt(index) + 1) * 2;
-    var space = parseInt(line) + 1
+    var space = parseInt(line) + 1;
     $('#assign tbody').append($('#template tbody').html().replace(/__prefix__/g, index));
-    $("#assign").find("tr:eq("+line+")").append($('#deletebutton').html())
-    $("#assign").find("tr:eq("+space+")").append('<hr>')
+    $("#deletebutton a").attr('id', index);
+    $("#assign").find("tr:eq("+line+")").append($('#deletebutton').html());
+    $("#assign").find("tr:eq("+space+")").append('<hr>');
     $('#id_form-TOTAL_FORMS').val(parseInt(index) + 1);
     if (month == 12){
       month = 1;
@@ -25,6 +26,41 @@ $(document).ready(function() {
       month ++;
     }
     $("#id_form-"+index+"-month_0").val(month);
-    $("#id_form-"+index+"-month_1").val(year)
+    $("#id_form-"+index+"-month_1").val(year);
+  });
+
+  $("#assign").on('click','.mydelete', function(){
+    var index = $("#id_form-TOTAL_FORMS").val();
+    var elt = $(this).closest('tr');
+    if (elt.next().is(":last-child")){
+      $("#id_form-TOTAL_FORMS").val(parseInt(index) - 1);
+      elt.next().remove();
+      elt.remove();
+    }else{
+      $("#id_form-TOTAL_FORMS").val(parseInt(index) - 1);
+      index = $("#id_form-TOTAL_FORMS").val();
+      var id = $(this).attr('id');
+      var eltiter = elt.next().next();
+      elt.next().remove();
+      elt.remove();
+      var iter = 0;
+      total = (index-id)*2
+      while (iter < total){
+        if (iter % 2 == 0){
+          eltiter.find('th label').attr('for', "id_form-"+id+"-month_0");
+          eltiter.find('td .w-month-year').attr({'name': "form-"+id+"-month_0", 'id': "id_form-"+id+"-month_0"});
+          eltiter.find('td .w-year').attr({'name': "form-"+id+"-month_1", 'id': "id_form-"+id+"-month_1"})
+          eltiter.find('a').attr('id', id)
+          iter ++;
+          eltiter = eltiter.next()
+        }else{
+          eltiter.find('th label').attr('for', "id_form-"+id+"-pourcentage");
+          eltiter.find('td select').attr({'name': "form-"+id+"-pourcentage", 'id': "id_form-"+id+"-pourcentage"})
+          id ++;
+          iter ++;
+          eltiter = eltiter.next()
+        }
+      }
+    }
   });
 });
