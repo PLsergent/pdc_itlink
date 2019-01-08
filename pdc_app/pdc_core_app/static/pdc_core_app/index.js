6642a0dc6c9292b5109a8caa2c96c55fd2f20793
plsergent <papa.louis59@gmail.com>
2019-01-08 10:17:30 +0100

    smooth animation index page

---
+++ pdc_app/pdc_core_app/static/pdc_core_app/index.js
@@ -0,0 +1,16 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal({ duration: 1500 });
+ScrollReveal().reveal('.first', {delay: 100 });
+ScrollReveal().reveal('.second', {delay: 250 });
+
+ScrollReveal({ duration: 4000 });
+ScrollReveal().reveal('.third', {delay: 300 });
+
+ScrollReveal({ duration: 1500 });
+ScrollReveal().reveal('.fourth', {delay: 300 });
+ScrollReveal().reveal('.fifth', {delay: 400 });
+ScrollReveal().reveal('.sixth', {delay: 500 });
+ScrollReveal().reveal('.seventh', {delay: 600 });
+ScrollReveal().reveal('.eighth', {delay: 700 });
+ScrollReveal().reveal('.ninth', {delay: 800 });
